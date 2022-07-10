7683c3c353821b09814e18743e9507bff5829344
yoon1024 <1anvin81@gmail.com>
2022-07-10 23:59:37 +0900

    main_visual

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function Visual() {
 	const path = process.env.PUBLIC_URL;
-	return <figure id='visual'></figure>;
+	return (
+		<figure id='visual'>
+			<img src={`${path}/img/main_visual.jpg`} alt='커피를 따르는 남자의 손' />
+			<div className='inner'>
+				<div className='title'>
+					<h2>I'VE BEEN FEELING</h2>
+					<h1>UNDER PRESSURE</h1>
+				</div>
+				<h3>Lorem ipsum dolor sit amet consectetur.</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet
+					obcaecati eaque possimus deleniti esse quidem vero laborum aliquid
+					assumenda accusantium!
+				</p>
+			</div>
+		</figure>
+	);
 }
 
 export default Visual;
