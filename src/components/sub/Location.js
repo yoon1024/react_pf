b088e9f7f02d66bc67981c372a07c15563a06bdf
yoon1024 <1anvin81@gmail.com>
2022-07-13 00:08:46 +0900

    마커 이미지 불러오기

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -9,15 +9,26 @@ function Location() {
 		level: 3,
 	};
 
-	const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+	//마커 이미지 변경에 필요한 3 정보 등록
+	const imageSrc = `${process.env.PUBLIC_URL}/img/coffee.png`;
+	const imageSize = new kakao.maps.Size(428, 494);
+	const imageOption = { offset: new kakao.maps.Point(214, 247) };
 
+	//위으 정보값을 토대로 마커 이미지 인스턴스 생성
+	const markerImage = new kakao.maps.MarkerImage(
+		imageSrc,
+		imageSize,
+		imageOption
+	);
+
+	const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
 	const marker = new kakao.maps.Marker({
 		position: markerPosition,
+		image: markerImage,
 	});
 
 	useEffect(() => {
 		const map_instance = new kakao.maps.Map(container.current, option);
-
 		marker.setMap(map_instance);
 	}, []);
 
