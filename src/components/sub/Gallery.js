77d654ce87925ade20af8f4c09545704ca784991
yoon1024 <1anvin81@gmail.com>
2022-07-18 02:31:00 +0900

    input

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -5,20 +5,25 @@ import Masonry from 'react-masonry-component';
 
 function Gallery() {
 	const frame = useRef(null);
+	const input = useRef(null);
 	const [Items, setItems] = useState([]);
 	const [Loading, setLoading] = useState(true);
 	const [EnableClick, setEnableClick] = useState(true);
 	const masonryOptions = { transitionDuration: '0.5s' };
 
-	const key = 'fc209af65c0a7e52e3ce3a2358041834';
+	const getFlickr = async (opt) => {
+		const key = 'fc209af65c0a7e52e3ce3a2358041834';
+		const method_interest = 'flickr.interestingness.getList';
+		const method_search = 'flickr.photos.search';
+		const num = 100;
+		let url = '';
 
-	const method_interest = 'flickr.interestingness.getList';
-	const method_search = 'flickr.photos.search';
-	const num = 100;
-	const interest_url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
-	const search_url = `https://www.flickr.com/services/rest/?method=${method_search}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&tags=${'coffee'}`;
-
-	const getFlickr = async (url) => {
+		if (opt.type === 'interest') {
+			url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
+		}
+		if (opt.type === 'search') {
+			url = `https://www.flickr.com/services/rest/?method=${method_search}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&tags=${opt.tags}`;
+		}
 		await axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
@@ -27,11 +32,14 @@ function Gallery() {
 		setTimeout(() => {
 			setLoading(false);
 			frame.current.classList.add('on');
+			setTimeout(() => {
+				setEnableClick(true);
+			}, 500);
 		}, 1000);
 	};
 
 	useEffect(() => {
-		getFlickr(interest_url);
+		getFlickr({ type: 'interest' });
 	}, []);
 
 	return (
@@ -44,20 +52,49 @@ function Gallery() {
 			)}
 			<button
 				onClick={() => {
+					if (!EnableClick) return;
+					setEnableClick(false);
 					setLoading(true);
 					frame.current.classList.remove('on');
-					getFlickr(interest_url);
+					getFlickr({ type: 'interest' });
 				}}>
 				Interest Gallery
 			</button>
 			<button
 				onClick={() => {
+					if (!EnableClick) return;
+					setEnableClick(false);
 					setLoading(true);
 					frame.current.classList.remove('on');
-					getFlickr(search_url);
+					getFlickr({ type: 'search', tags: 'coffee' });
 				}}>
 				Search Gallery
 			</button>
+
+			<div className='searchBox'>
+				<input
+					type='text'
+					ref={input}
+					placeholder='Please enter a search word'
+				/>
+				<button
+					onClick={() => {
+						const result = input.current.value;
+						input.current.value = '';
+
+						if (!EnableClick) return;
+						setEnableClick(false);
+						setLoading(true);
+						frame.current.classList.remove('on');
+						getFlickr({
+							type: 'search',
+							tags: result,
+						});
+					}}>
+					SEARCH
+				</button>
+			</div>
+
 			<Masonry elementType={'div'} options={masonryOptions}>
 				<div className='frame' ref={frame}>
 					{Items.map((item, idx) => {
